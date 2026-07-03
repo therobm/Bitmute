e7a421caf307432d76b34c7cbc7256bfb8d7c618
Rob M <rob@mccoder.com>
2026-07-03 04:23:37 -0400

    Persist light/dark theme choice across launches

--- Bitmute/UI/Theme.cs
+++ Bitmute/UI/Theme.cs
@@ -2,12 +2,18 @@ using System;
 using Microsoft.Maui;
 using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
 using SkiaSharp;
 
 namespace Bitmute.UI
 {
 	public static class Theme
 	{
+		private const string ModeKey = "theme_mode";
+		private const string ModeSystem = "system";
+		private const string ModeDark = "dark";
+		private const string ModeLight = "light";
+
 		private static bool s_dark = true;
 		private static bool s_followSystem = true;
 
@@ -23,10 +29,62 @@ namespace Bitmute.UI
 			return application.RequestedTheme != AppTheme.Light;
 		}
 
+		private static void ApplyToApplication()
+		{
+			Application application = Application.Current;
+			if (application == null)
+			{
+				return;
+			}
+			if (s_followSystem)
+			{
+				application.UserAppTheme = AppTheme.Unspecified;
+				return;
+			}
+			if (s_dark)
+			{
+				application.UserAppTheme = AppTheme.Dark;
+				return;
+			}
+			application.UserAppTheme = AppTheme.Light;
+		}
+
+		private static void Save()
+		{
+			string mode = ModeSystem;
+			if (!s_followSystem)
+			{
+				if (s_dark)
+				{
+					mode = ModeDark;
+				}
+				else
+				{
+					mode = ModeLight;
+				}
+			}
+			Preferences.Default.Set(ModeKey, mode);
+		}
+
 		public static void InitializeFromSystem()
 		{
-			s_followSystem = true;
-			s_dark = SystemIsDark();
+			string mode = Preferences.Default.Get(ModeKey, ModeSystem);
+			if (mode == ModeDark)
+			{
+				s_followSystem = false;
+				s_dark = true;
+			}
+			else if (mode == ModeLight)
+			{
+				s_followSystem = false;
+				s_dark = false;
+			}
+			else
+			{
+				s_followSystem = true;
+				s_dark = SystemIsDark();
+			}
+			ApplyToApplication();
 		}
 
 		public static bool IsDark()
@@ -51,36 +109,27 @@ namespace Bitmute.UI
 		public static void UseSystem()
 		{
 			s_followSystem = true;
-			Application application = Application.Current;
-			if (application != null)
-			{
-				application.UserAppTheme = AppTheme.Unspecified;
-			}
 			s_dark = SystemIsDark();
+			ApplyToApplication();
+			Save();
 			Fire();
 		}
 
 		public static void UseDark()
 		{
 			s_followSystem = false;
-			Application application = Application.Current;
-			if (application != null)
-			{
-				application.UserAppTheme = AppTheme.Dark;
-			}
 			s_dark = true;
+			ApplyToApplication();
+			Save();
 			Fire();
 		}
 
 		public static void UseLight()
 		{
 			s_followSystem = false;
-			Application application = Application.Current;
-			if (application != null)
-			{
-				application.UserAppTheme = AppTheme.Light;
-			}
 			s_dark = false;
+			ApplyToApplication();
+			Save();
 			Fire();
 		}
 
